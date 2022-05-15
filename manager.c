8855348239775be0d252c239b5029370261f3787
김성경 <kimseongkyeong@gimseong-gyeong-ui-MacBookPro.local>
2022-05-16 07:17:36 +0900

    create save&load function

--- manager.c
+++ manager.c
@@ -19,3 +19,36 @@ int selectDataNum(Car *s, int count){
  scanf("%d", &num);
  return num;
 }
+
+void saveData(Car slist[], int count) {
+ FILE* fp;
+ fp = fopen("car.txt", "wt");
+ for(int i=0; i<count; i++) {
+	if(slist[i].price==-1) continue;
+	fprintf(fp, "%s %s %s %d %d %d\n", slist[i].name, slist[i].carName, slist[i].location, slist[i].price, slist[i].rentalTime, slist[i].rentalType);
+ }
+ 
+ fclose(fp);
+ printf("¿¿¿!\n);  
+
+}
+
+int loadData(Car *slist)  {
+ int count=0;
+ FILE* fp;
+ fp = fopen("car.txt", "r");
+ if(fp==NULL) printf("\n=> ¿¿¿¿\n");
+ else {
+	for(int i=0; i<100; i++) {
+               fscanf( fp, "%s %s %s %d %d %d\n", slist[i].name, slist[i].carName, slist[i].location, &slist[i].price, &slist[i].rentalTime, &slist[i].rentalType);
+		if(feof(fp)) break;
+		count++;
+ 	}
+	fclose(fp);
+ printf("=>¿¿¿¿!\n");
+ }
+
+ return count;
+
+}
+
